6c6f87e9d63616aa3f951ee5079486ec19f27bc4
w <sjtuwyf@126.com>
2022-02-25 22:16:22 +0800

    20220225-wyf-netty-2-11-数据流切块传输

---
+++ itstack-demo-netty-2-11/src/main/java/org/itstack/demo/netty/server/MyChannelInitializer.java
@@ -0,0 +1,31 @@
+package org.itstack.demo.netty.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+import java.nio.charset.Charset;
+
+/**
+ * @author ssqswyf
+ * @date 2022/2/25
+ */
+public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel channel) throws Exception {
+        // 基于换行符号
+        channel.pipeline().addLast(new LineBasedFrameDecoder(1024));
+        // 流量分块
+        channel.pipeline().addLast(new ChunkedWriteHandler());
+        channel.pipeline().addLast(new MyServerChunkHandler());
+        // 解码转String，注意调整自己的编码格式GBK、UTF-8
+        channel.pipeline().addLast(new StringDecoder(Charset.forName("GBK")));
+        // String转编码，注意调整自己的编码格式GBK、UTF-8
+        channel.pipeline().addLast(new StringEncoder(Charset.forName("GBK")));
+        // 在管道中添加我们自己的接收数据实现方法
+        channel.pipeline().addLast(new MyServerHandler());
+    }
+}
